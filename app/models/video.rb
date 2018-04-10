e9b09ee093d02997d46e30da2e57963beeecc772
patbrennan <patrick@junecommerce.org>
2018-04-10 19:12:47 +0000

    start review feature - db schema, model, validations, tests

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,16 @@
 class Video < ActiveRecord::Base
   belongs_to :category
+  has_many :reviews, -> { order("created_at DESC") }
 
   validates :title, :description, presence: true
 
   def self.search_by_title(string)
     return [] if string.blank?
     where("title LIKE ?", "%#{string}%").order("created_at DESC")
+  end
+
+  def avg_rating
+    reviews = self.reviews
+    self.reviews.average(:rating).to_f
   end
 end
---
+++ spec/models/review_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Review do
+  it { should belong_to :user }
+  it { should belong_to :video }
+  it { should validate_presence_of :rating }
+  it { should validate_presence_of :description }
+  it { should validate_inclusion_of(:rating).in_array([1, 2, 3, 4, 5]) }
+
+  it "doesn't save when rating is < 1 or > 5"
+end
